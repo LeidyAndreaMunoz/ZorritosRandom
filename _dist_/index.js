171b0d1cbba7164015ec4bdee0d18e6fb7bb3a7a
LeidyAndreaMunoz <LeidyAndreaMunoz@users.noreply.github.com>
2021-07-15 01:09:20 +0000

    deploy: cdf49f96b0a303d659c18faddb3779fa7ca8c6f4

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,6 +1,42 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { registerImage } from "./lazy.js";
 
-console.log('Happy hacking :)')
+const maximum = 122;
+const minimum = 1;
+
+
+const random = () => Math.floor(Math.random() * (maximum - minimum)) + minimum;
+
+const createImageNode = () => {
+
+    const container = document.createElement('div')
+    container.className = "p-4 bg-gradient-to-r from-red-200 rounded-2xl m-px";
+
+    const imagen = document.createElement('img')
+    imagen.className = "mx-auto rounded-2xl";
+    imagen.width= '320'
+    imagen.dataset.src = `https://randomfox.ca/images/${random()}.jpg`;
+
+    container.appendChild(imagen)
+
+    return container;
+};
+
+const nuevaImagen = createImageNode();
+const mountNode = document.getElementById("images");
+
+const containerImages = document.querySelector('#images');
+
+const addButton = document.querySelector('#Add_button')
+const addImage = () => {
+    const newImage = createImageNode();
+    mountNode.appendChild(newImage);
+    registerImage(newImage);
+};
+
+const cleanButton = document.querySelector('#reset')
+const cleanImages = () => {
+    containerImages.innerHTML = " ";
+};
+
+addButton.addEventListener("click", addImage);
+cleanButton.addEventListener("click", cleanImages);
